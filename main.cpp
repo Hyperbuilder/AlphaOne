eb7b6a2e8fe4b6fc6c1db19fa9061a2a8922ce61
quaqiem <106615@sevenwolden.org>
2022-11-24 14:20:02 +0100

    Commit

--- main.cpp
+++ main.cpp
@@ -4,5 +4,5 @@ using namespace std;
 int main() {
     cout << "Hello, World!";
     return 0;
-    //ur gay
+    //https://code-with-me.global.jetbrains.com/tPcKzJxOZy6KcVZjzZPfDA#p=CL&fp=A84C6243CE5CFB76E8FA0CD06BB492F17BC8CED7594F3B247E410CC4E25FE378
 }
